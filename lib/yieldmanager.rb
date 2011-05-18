6464b05e35fa895057c0d547a2189bfa2913fb5f
Bill Gathen <bill@billgathen.com>
2011-05-18 12:00:44 -0400

    Added soap4r_19_patch to implement relevant parts of Tomer Doron's "Fixing soap4r for ruby 1.9". Specs now complete with rspec1 in 1.8.x and rspec2 in 1.9.x.

--- lib/yieldmanager.rb
+++ lib/yieldmanager.rb
@@ -12,6 +12,14 @@ end
 require 'patch_detector'
 include PatchDetector
 
+if (RUBY_VERSION.start_with?("1.9"))
+  #
+  # This patch based on Tomer Doron's "Fixing soap4r for ruby 1.9" post
+  # http://tomerdoron.blogspot.com/2009/10/fixing-soap4r-for-ruby-19.html
+  #
+  $LOAD_PATH.unshift 'lib/soap4r_19_patch'
+end
+
 require 'yieldmanager/client'
 
 if needs_patching?(:ruby_version => RUBY_VERSION, :minimum_ruby_version_for_patch => "1.8.7")
